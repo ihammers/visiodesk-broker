020d58263196f77fb45404c2109eccaf14d15db3
John <e.k@mail.ru>
2021-05-09 21:41:00 +0300

    ssl

--- conf/nginx/ssl/ssl.sh
+++
@@ -1,4 +0,0 @@
-cd /opt/services/conf/nginx/ssl/
-openssl genrsa -aes256 -out nginx.key 1024
-openssl req -new -key nginx.key -out nginx.csr
-openssl x509 -req -days 365 -in nginx.csr -signkey nginx.key -out nginx.crt
